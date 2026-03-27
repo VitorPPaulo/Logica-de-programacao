5a0fa5474200cd7021566956186b16a50a4d6022
VitorPPaulo <vitor.petrica.p@gmail.com>
2026-03-27 20:26:09 -0300

    Add files via upload

---
+++ Calculadora de area.cpp
@@ -0,0 +1,19 @@
+#include<conio.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<locale.h>
+
+int main ()
+{
+	//variaveis
+	setlocale (LC_ALL,"portuguese");
+	float a, pi, r=0;
+	//inicio
+	pi=3.1415;
+	printf("Entre com o raio:\n");
+	scanf("%f", &r);
+	a=pi*pow(r,2);
+	printf("Valor da area:  %.4f\n", a);
+	getch();
+}
