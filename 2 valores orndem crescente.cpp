4b382869070b50190e8ccd731057038086aa0af6
VitorPPaulo <vitor.petrica.p@gmail.com>
2026-04-10 20:05:31 -0300

    Create 2 valores orndem crescente.cpp

---
+++ 2 valores orndem crescente.cpp
@@ -0,0 +1,23 @@
+#include<conio.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<locale.h>
+
+int main ()
+{
+	//variaveis
+	setlocale (LC_ALL,"portuguese");
+	float valor1, valor2, valor3, valor4=0;
+	//inicio
+	printf("Entre com o primeiro valor\n");
+	scanf("%f", &valor1);
+	printf("Entre com o segundo valor\n");
+	scanf("%f", &valor2);
+	if(valor1 < valor2) {
+		printf("%.0f, %.0f", valor1, valor2);
+		}
+			if(valor2 < valor1){
+				printf("%.0f, %.0f", valor2, valor1);
+			}
+}
