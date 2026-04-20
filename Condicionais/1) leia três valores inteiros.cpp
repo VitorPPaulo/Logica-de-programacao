e0430ed574902e65f8bac9b6a9c077511046f3aa
VitorPPaulo <vitor.petrica.p@gmail.com>
2026-04-20 13:03:33 -0300

    pasta

---
+++ Condicionais/1) leia três valores inteiros.cpp
@@ -0,0 +1,31 @@
+#include<conio.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<locale.h>
+
+int main ()
+{
+	//variaveis
+	setlocale (LC_ALL,"portuguese");
+	int valor1, valor2, valor3, valor4=0;
+	//inicio
+	printf("Entre com o primeiro valor\n");
+	scanf("%i", &valor1);
+	printf("Entre com o segundo valor\n");
+	scanf("%i", &valor2);
+	printf("Entre com o terceiro valor\n");
+	scanf("%i", &valor3);
+	valor4 = valor1;
+	if(valor1 < valor4) {
+		valor4 = valor1;
+	}
+		if (valor2 < valor4) {
+    		    valor4 = valor2;
+    	}
+    		if (valor3 < valor4) {
+        		valor4 = valor3;
+    		}
+    printf("O menor valor é: %i\n", valor4);
+	getch();
+}//fim
